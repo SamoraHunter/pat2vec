5af7c76dcde5688442963a161ca6dfab368a7f91
SamoraHunter <samorahunter@hotmail.co.uk>
2024-01-02 15:13:22 +0000

    added post processing multi processing concatentation function

--- util/post_processing.py
+++ util/post_processing.py
@@ -2,6 +2,7 @@ import csv
 import os
 import sys
 from datetime import datetime
+from multiprocessing import Pool
 
 import pandas as pd
 from tqdm import tqdm
@@ -17,6 +18,9 @@ def count_files(path):
     for root, dirs, files in os.walk(path):
         count += len(files)
     return count
+
+
+
 
 
 def process_csv_files(input_path, out_folder='outputs', output_filename_suffix='concatenated_output',  part_size=336, sample_size = None, append_timestamp_column=False):
@@ -59,7 +63,7 @@ def process_csv_files(input_path, out_folder='outputs', output_filename_suffix='
 
     # Loop through each CSV file and read its data
     if not curate_columns:
-        for file in tqdm(all_files):
+        for file in (all_files):
             if file.endswith('.csv'):
                 with open(file, 'r', newline='') as infile:
                     reader = csv.reader(infile)
@@ -77,7 +81,7 @@ def process_csv_files(input_path, out_folder='outputs', output_filename_suffix='
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         base_name, extension = os.path.splitext(output_file)
         new_output_file = f"{base_name}_{timestamp}_overwritten{extension}"
-        print(f"Warning: Output file already exists. Renaming to {new_output_file}")
+        print(f"Warning: Output file already exists. Renaming {output_file} to {new_output_file}")
         os.rename(output_file, new_output_file)
     else:
         new_output_file = output_file
@@ -122,6 +126,8 @@ def process_csv_files(input_path, out_folder='outputs', output_filename_suffix='
         df.to_csv(output_file)
 
     print(f"Concatenated data saved to {output_file}")
+    
+    return output_file
 
 # Example Usage:
 # concatenate_csv_files('/home/cogstack/samora/_data/HAEM_AG11193_3/new_project/current_pat_lines_parts', 'output_path_here')
@@ -378,3 +384,81 @@ def remove_file_from_paths(current_pat_idcode: str, project_name='new_project' )
             print(path + current_pat_idcode + ".csv", "successfully removed")
         except:
             print(path + current_pat_idcode + ".csv", "not found")
+
+
+
+def process_chunk(args):
+    part_chunk, all_files, part_size, unique_columns = args
+    concatenated_data = {column: [] for column in unique_columns}
+    for file in all_files[part_chunk:part_chunk + part_size]:
+        if file.endswith('.csv'):
+            with open(file, 'r', newline='') as infile:
+                reader = csv.DictReader(infile)
+                for row in reader:
+                    for column in unique_columns:
+                        concatenated_data[column].append(row.get(column, ''))
+    return concatenated_data
+
+def process_csv_files_multi(input_path, out_folder='outputs', output_filename_suffix='concatenated_output', part_size=336, sample_size=None, append_timestamp_column=False):
+    curate_columns = False
+
+    all_file_paths = [os.path.join(dp, f) for dp, dn, filenames in os.walk(input_path) for f in filenames if os.path.splitext(f)[1] == '.csv']
+
+    if type(sample_size) == str or sample_size is None:
+        if sample_size is None or sample_size.lower() == 'all':
+            sample_size = len(all_file_paths)
+
+    output_file = os.path.join(out_folder, f'concatenated_data_{output_filename_suffix}.csv')
+
+    unique_columns = set()
+
+    all_files = all_file_paths if sample_size is None else all_file_paths[:sample_size]
+
+    print("all files size", len(all_files))
+
+    if not curate_columns:
+        for file in all_files:
+            if file.endswith('.csv'):
+                with open(file, 'r', newline='') as infile:
+                    reader = csv.reader(infile)
+                    try:
+                        header = next(reader)
+                        unique_columns.update(header)
+                    except StopIteration:
+                        pass
+
+    if os.path.exists(output_file):
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        base_name, extension = os.path.splitext(output_file)
+        new_output_file = f"{base_name}_{timestamp}_overwritten{extension}"
+        print(f"Warning: Output file already exists. Renaming {output_file} to {new_output_file}")
+        os.rename(output_file, new_output_file)
+    else:
+        new_output_file = output_file
+
+    unique_columns = list(unique_columns)
+    unique_columns.sort(key=lambda x: x != 'client_idcode')
+
+    with open(output_file, 'w', newline='') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames=unique_columns)
+        writer.writeheader()
+
+    with Pool() as pool:
+        args_list = [(i, all_files, part_size, unique_columns) for i in range(0, len(all_files), part_size)]
+        results = list(tqdm(pool.imap(process_chunk, args_list), total=len(all_files)//part_size))
+
+    with open(output_file, 'a', newline='') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames=unique_columns)
+        for result in tqdm(results, desc='Writing lines...'):
+            for i in range(len(result[next(iter(result))])):
+                writer.writerow({column: result[column][i] for column in unique_columns})
+
+    if append_timestamp_column:
+        print("Reading results and appending updatetime column")
+        df = pd.read_csv(output_file)
+        df = extract_datetime_to_column(df)
+        df.to_csv(output_file)
+
+    print(f"Concatenated data saved to {output_file}")
+
+    return output_file
