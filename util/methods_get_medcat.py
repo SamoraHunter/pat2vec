2d1778c5c6e888a226f9e226366e6895b446c133
SamoraHunter <samorahunter@hotmail.co.uk>
2024-01-16 22:12:43 +0000

    added override medcat model path

--- util/methods_get_medcat.py
+++ util/methods_get_medcat.py
@@ -6,8 +6,9 @@ def get_cat(config_obj):
     if (config_obj.medcat):
 
         from medcat.cat import CAT
-
-        if config_obj.testing:
+        if(config_obj.override_medcat_model_path is not None):
+            model_path = config_obj.override_medcat_model_path
+        elif config_obj.testing:
             model_path = 'medcat_models\medcat_model_pack_422d1d38fc58f158.zip'
         elif config_obj.aliencat:
             model_path = '/home/aliencat/samora/HFE/HFE/medcat_models/medcat_model_pack_316666b47dfaac07.zip'
