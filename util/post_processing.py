ca15f0bb02a51d6e9b1736f3e4790d01d44739aa
SamoraHunter <samorahunter@hotmail.co.uk>
2024-02-04 19:29:27 +0000

    added post processing build methods, build merged annotations

--- util/post_processing.py
+++ util/post_processing.py
@@ -1,15 +1,16 @@
 import csv
 import os
+import shutil
 import sys
 from datetime import datetime
+from itertools import chain
 from multiprocessing import Pool, cpu_count
+from pathlib import Path
+from typing import Dict, List, Optional, Union
+
 import pandas as pd
 from IPython.display import display
 from tqdm import tqdm
-import shutil
-from typing import Optional, Union, Dict, List
-from itertools import chain
-
 
 sys.path.insert(0, '/home/aliencat/samora/gloabl_files')
 sys.path.insert(0, '/data/AS/Samora/gloabl_files')
@@ -902,3 +903,25 @@ def get_all_target_annots(all_pat_list, n_lists, config_obj=None, annot_filter_a
     results_df.to_csv('all_target_annots.csv')
 
     return results_df
+
+
+def build_merged_epr_mct_annot_df(config_obj):
+    directory_path = config_obj.proj_name + "/" + "merged_batches/"
+    Path(directory_path).mkdir(parents=True, exist_ok=True)
+
+    output_file_path = directory_path + 'annots_mct_epr.csv'
+
+    all_pat_list = config_obj.all_pat_list
+
+    for i in tqdm(range(0, len(all_pat_list)), total=len(all_pat_list)):
+        current_pat_idcode = all_pat_list[i]
+        all_annots = retrieve_pat_annots_mct_epr(
+            current_pat_idcode, config_obj)
+
+        if i == 0:
+            # Create the output file and write the first batch directly
+            all_annots.to_csv(output_file_path, index=False)
+        else:
+            # Append each result to the output file
+            all_annots.to_csv(output_file_path, mode='a',
+                              header=False, index=False)
