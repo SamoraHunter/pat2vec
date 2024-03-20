f805165b6116e4321c73e04691881b63b6a26605
Samora Hunter <samorahunter@hotmail.co.uk>
2024-03-20 09:46:25 +0000

    moved filter methods outside main batch methods

--- pat2vec/util/filter_methods.py
+++ pat2vec/util/filter_methods.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from fuzzywuzzy import process
-
+from pat2vec.util.methods_annotation_regex import append_regex_term_counts
+from IPython.display import display
 
 def filter_dataframe_by_fuzzy_terms(
     df, filter_term_list, column_name="document_description", verbose=0
@@ -35,3 +36,75 @@ def filter_dataframe_by_fuzzy_terms(
 
     filtered_df = df[df.index.isin(matched_indices)]
     return filtered_df
+
+
+def apply_data_type_epr_docs_filters(config_obj, batch_target):
+    """
+    Apply data type filters to the batch target based on the configuration object.
+
+    Args:
+        config_obj (object): Configuration object containing data type filter information.
+        batch_target (DataFrame): Batch target DataFrame to be filtered.
+
+    Returns:
+        DataFrame: Filtered batch target DataFrame.
+
+    """
+    if config_obj.data_type_filter_dict is not None and not batch_target.empty:
+        if config_obj.data_type_filter_dict.get("filter_term_lists").get("epr_docs") is not None:
+            if config_obj.verbosity >= 1:
+                print("Applying document type filter to EPR documents", config_obj.data_type_filter_dict)
+            filter_term_list = config_obj.data_type_filter_dict.get("filter_term_lists").get("epr_docs")
+            batch_target = filter_dataframe_by_fuzzy_terms(
+                batch_target,
+                filter_term_list,
+                column_name="document_description",
+                verbose=config_obj.verbosity,
+            )
+
+        if config_obj.data_type_filter_dict.get("filter_term_lists").get("epr_docs_term_regex") is not None:
+            if config_obj.verbosity > 1:
+                print("Appending regex term counts...")
+                if config_obj.verbosity > 5:
+                    display(batch_target)
+            batch_target = append_regex_term_counts(
+                df=batch_target,
+                terms=config_obj.data_type_filter_dict.get("filter_term_lists").get("epr_docs_term_regex"),
+                text_column="body_analysed",
+                debug=config_obj.verbosity > 5,
+            )
+    else:
+        if config_obj.verbosity >= 1:
+            print("Data type filter dictionary is None or batch target is empty. No filtering applied.")
+    return batch_target
+
+
+def apply_bloods_data_type_filter(config_obj, batch_target):
+    """
+    Apply data type filter specific to bloods to the batch target based on the configuration object.
+
+    Args:
+        config_obj (object): Configuration object containing data type filter information.
+        batch_target (DataFrame): Batch target DataFrame to be filtered.
+
+    Returns:
+        DataFrame: Filtered batch target DataFrame.
+
+    """
+    if config_obj.data_type_filter_dict is not None:
+        if config_obj.data_type_filter_dict.get("filter_term_lists").get("bloods") is not None:
+            if config_obj.verbosity >= 1:
+                print("Applying document type filter to bloods", config_obj.data_type_filter_dict)
+            filter_term_list = config_obj.data_type_filter_dict.get("filter_term_lists").get("bloods")
+            batch_target = filter_dataframe_by_fuzzy_terms(
+                batch_target,
+                filter_term_list,
+                column_name="basicobs_itemname_analysed",
+                verbose=config_obj.verbosity,
+            )
+    else:
+        if config_obj.verbosity >= 1:
+            print("Data type filter dictionary is None. No filtering applied.")
+    return batch_target
+
+
