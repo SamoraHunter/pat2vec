1df36188b4505158282b5f21b73112e59998481a
SamoraHunter <samorahunter@hotmail.co.uk>
2024-04-15 19:22:24 +0100

    updated method

--- pat2vec/pat2vec_get_methods/get_method_pat_annotations.py
+++ pat2vec/pat2vec_get_methods/get_method_pat_annotations.py
@@ -1,4 +1,3 @@
-
 import os
 import pickle
 import time
@@ -11,16 +10,42 @@ from IPython.utils import io
 
 from pat2vec.util.methods_annotation import (
     calculate_pretty_name_count_features,
-    check_pat_document_annotation_complete, filter_annot_dataframe,
-    get_pat_document_annotation_batch)
-from pat2vec.util.methods_get import (dump_results, exist_check,
-                                      filter_dataframe_by_timestamp,
-                                      get_start_end_year_month, update_pbar)
+    check_pat_document_annotation_complete,
+    filter_annot_dataframe,
+    get_pat_document_annotation_batch,
+)
+from pat2vec.util.methods_get import (
+    dump_results,
+    exist_check,
+    filter_dataframe_by_timestamp,
+    get_start_end_year_month,
+    update_pbar,
+)
 
 
-def get_current_pat_annotations(current_pat_client_id_code, target_date_range, batch_epr_docs_annotations, config_obj=None, t=None, cohort_searcher_with_terms_and_search=None, cat=None):
+def get_current_pat_annotations(
+    current_pat_client_id_code,
+    target_date_range,
+    batch_epr_docs_annotations,
+    config_obj=None,
+    t=None,
+    cohort_searcher_with_terms_and_search=None,
+    cat=None,
+):
     """
     Retrieves and processes annotations for a specific patient within a given date range.
+
+    This function is responsible for retrieving annotations for a single patient
+    within a given time range. It takes in a configuration object, a target date
+    range, a DataFrame containing EPR document annotations, and an optional cohort
+    searcher and medcat object.
+
+    The function uses the provided configuration to set up parameters such as start
+    time and verbosity level. It then filters the batch_epr_docs_annotations DataFrame
+    based on the target_date_range. If filtered annotations exist, it calculates
+    pretty name count features and returns the resulting DataFrame. If not, it creates
+    a DataFrame with the client_idcode. The resulting DataFrame is displayed if the
+    verbosity level is 6 or higher.
 
     Parameters:
     - current_pat_client_id_code (str): The unique identifier for the patient.
@@ -36,58 +61,81 @@ def get_current_pat_annotations(current_pat_client_id_code, target_date_range, b
 
     Raises:
     - ValueError: If config_obj is None, a valid configuration must be provided.
+    - TypeError: If batch_epr_docs_annotations is not a pd.DataFrame.
 
-    Notes:
-    - The function uses the provided configuration to set up parameters such as start time and verbosity level.
-    - It filters the batch_epr_docs_annotations DataFrame based on the target_date_range.
-    - If filtered annotations exist, it calculates pretty name count features; otherwise, it creates a DataFrame with the client_idcode.
-    - The resulting DataFrame is displayed if the verbosity level is 6 or higher.
-
-    Example:
-    ```python
-    annotations = get_current_pat_annotations('patient123', '(2023,1,1)', batch_epr_docs_annotations, config_obj)
-    ```
     """
+
     if config_obj is None:
         raise ValueError(
-            "config_obj cannot be None. Please provide a valid configuration. (get_current_pat_annotations)")
+            "config_obj cannot be None. Please provide a valid configuration. (get_current_pat_annotations)"
+        )
+
+    if batch_epr_docs_annotations is not None and not isinstance(
+        batch_epr_docs_annotations, pd.DataFrame
+    ):
+        raise TypeError("batch_epr_docs_annotations must be a pd.DataFrame.")
 
     start_time = config_obj.start_time
 
-    p_bar_entry = 'annotations_epr'
+    p_bar_entry = "annotations_epr"
 
-    update_pbar(current_pat_client_id_code, start_time, 0,
-                p_bar_entry, t, config_obj, config_obj.skipped_counter)
+    update_pbar(
+        current_pat_client_id_code,
+        start_time,
+        0,
+        p_bar_entry,
+        t,
+        config_obj,
+        config_obj.skipped_counter,
+    )
 
-    start_year, start_month, end_year, end_month, start_day, end_day = get_start_end_year_month(
-        target_date_range, config_obj=config_obj)
+    # Extract start and end dates from the target date range
+    start_year, start_month, end_year, end_month, start_day, end_day = (
+        get_start_end_year_month(target_date_range, config_obj=config_obj)
+    )
 
-    if (batch_epr_docs_annotations is not None):
+    # Filter the batch_epr_docs_annotations DataFrame based on the target_date_range
+    if batch_epr_docs_annotations is not None:
 
-        filtered_batch_epr_docs_annotations = filter_dataframe_by_timestamp(batch_epr_docs_annotations,
-                                                                            start_year,
-                                                                            start_month,
-                                                                            end_year,
-                                                                            end_month,
-                                                                            start_day, end_day, 'updatetime', dropna=True)
+        # Filter the dataframe based on the target date range
+        filtered_batch_epr_docs_annotations = filter_dataframe_by_timestamp(
+            batch_epr_docs_annotations,
+            start_year,
+            start_month,
+            end_year,
+            end_month,
+            start_day,
+            end_day,
+            "updatetime",
+            dropna=True,
+        )
 
-        if (len(filtered_batch_epr_docs_annotations) > 0):
+        if len(filtered_batch_epr_docs_annotations) > 0:
 
+            # Calculate pretty name count features for the filtered dataframe
             df_pat_target = calculate_pretty_name_count_features(
-                filtered_batch_epr_docs_annotations)
+                filtered_batch_epr_docs_annotations
+            )
 
         else:
+            # If filtered annotations don't exist, create a DataFrame with the client_idcode
             if config_obj.verbosity >= 6:
-                print("len(filtered_batch_epr_docs_annotations)>0",
-                      len(filtered_batch_epr_docs_annotations) > 0)
+                print(
+                    "len(filtered_batch_epr_docs_annotations)>0",
+                    len(filtered_batch_epr_docs_annotations) > 0,
+                )
             df_pat_target = pd.DataFrame(
-                data=[current_pat_client_id_code], columns=['client_idcode'])
+                data=[current_pat_client_id_code], columns=["client_idcode"]
+            )
 
     else:
+        # If the batch_epr_docs_annotations DataFrame is None, create a DataFrame with the client_idcode
         df_pat_target = pd.DataFrame(
-            data=[current_pat_client_id_code], columns=['client_idcode'])
+            data=[current_pat_client_id_code], columns=["client_idcode"]
+        )
 
     if config_obj.verbosity >= 6:
+        # Display the processed DataFrame if the verbosity level is 6 or higher
         display(df_pat_target)
 
     return df_pat_target
