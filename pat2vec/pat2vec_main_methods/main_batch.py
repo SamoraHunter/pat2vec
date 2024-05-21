75c34aca708cb73e20ef1dc918c383572a6b0b7b
SamoraHunter <samorahunter@hotmail.co.uk>
2024-05-21 17:20:23 +0100

    error catching on vector size

--- pat2vec/pat2vec_main_methods/main_batch.py
+++ pat2vec/pat2vec_main_methods/main_batch.py
@@ -148,6 +148,7 @@ def main_batch(
                         batch_demo,
                         config_obj=config_obj,
                     )
+
                     patient_vector.append(current_pat_demo)
 
                 if main_options.get("bmi"):
@@ -369,6 +370,9 @@ def main_batch(
                 )
 
                 if remote_dump == False:
+                    if len(pat_concatted) > 1:
+                        print(pat_concatted)
+                        raise ValueError("Batch too large for local dump")
 
                     pat_concatted.to_csv(output_path)
                 else:
