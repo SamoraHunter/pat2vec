52ff99310d4a8d6fedd198294fa3629e4f9ab25d
SamoraHunter <samorahunter@hotmail.co.uk>
2025-02-19 17:06:40 +0000

    updated example

--- install_pat2vec.sh
+++ install_pat2vec.sh
@@ -83,7 +83,7 @@ clone_repositories() {
     local repos=(
         "https://github.com/SamoraHunter/cogstack_search_methods.git"
         "https://github.com/SamoraHunter/clinical_note_splitter.git"
-        "https://github.com/SamoraHunter/SNOMED_methods.git"
+        "https://github.com/SamoraHunter/snomed_methods.git"
         
     )
     
