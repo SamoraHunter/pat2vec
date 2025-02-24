cd96c7e01c468b4adf9152e3563612992dfee9d4
SamoraHunter <samorahunter@hotmail.co.uk>
2025-02-24 18:31:00 +0000

    path fix

--- notebooks/test_notebook.py
+++ notebooks/test_notebook.py
@@ -4,7 +4,7 @@ from nbconvert.preprocessors import ExecutePreprocessor
 
 
 def test_notebook():
-    with open("example_usage.ipynb") as f:
+    with open("notebooks/example_usage.ipynb") as f:
         nb = nbformat.read(f, as_version=4)
 
     ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
