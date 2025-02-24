ed1b1d7891a8472fae2c9e4d024db1700435f811
SamoraHunter <samorahunter@hotmail.co.uk>
2025-02-24 18:06:59 +0000

    initial notebook test

---
+++ notebooks/test_notebook.py
@@ -0,0 +1,11 @@
+import pytest
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+
+def test_notebook():
+    with open("example_usage.ipynb") as f:
+        nb = nbformat.read(f, as_version=4)
+
+    ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+    ep.preprocess(nb)  # Will raise an error if the notebook fails
