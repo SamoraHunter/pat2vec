69b36931f356e88f22edca76f912f566894d7a2c
SamoraHunter <samorahunter@hotmail.co.uk>
2025-02-28 13:35:58 +0000

    kernel name pat2vec_env

--- notebooks/test_notebook.py
+++ notebooks/test_notebook.py
@@ -7,5 +7,5 @@ def test_notebook():
     with open("notebooks/example_usage.ipynb") as f:
         nb = nbformat.read(f, as_version=4)
 
-    ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+    ep = ExecutePreprocessor(timeout=600, kernel_name="pat2vec_env")
     ep.preprocess(nb)  # Will raise an error if the notebook fails
