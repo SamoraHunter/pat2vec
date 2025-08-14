3efc3037fda28e4091e4c4f2686703f032e7fd2d
SamoraHunter <samorahunter@hotmail.co.uk>
2025-08-14 09:32:46 +0100

    adjusted notebook path method in test

--- notebooks/test_notebook.py
+++ notebooks/test_notebook.py
@@ -8,10 +8,21 @@ def test_notebook():
     notebook_filename = "notebooks/example_usage.ipynb"
     notebook_dir = os.path.dirname(notebook_filename)
 
+    # Get absolute path to ensure proper directory resolution
+    notebook_abs_path = os.path.abspath(notebook_filename)
+    notebook_abs_dir = os.path.dirname(notebook_abs_path)
+
     with open(notebook_filename) as f:
         nb = nbformat.read(f, as_version=4)
 
     ep = ExecutePreprocessor(timeout=600, kernel_name="pat2vec_env")
 
-    # Set the working directory for the notebook execution
-    ep.preprocess(nb, {"metadata": {"path": notebook_dir}})
+    # Change to the notebook directory before execution
+    original_cwd = os.getcwd()
+    try:
+        os.chdir(notebook_abs_dir)
+        # Execute the notebook with the correct working directory
+        ep.preprocess(nb, {"metadata": {"path": notebook_abs_dir}})
+    finally:
+        # Always restore the original working directory
+        os.chdir(original_cwd)
