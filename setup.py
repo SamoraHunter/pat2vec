d615fceefe46229a30ddf8c1f0e31fba4b07eab6
SamoraHunter <samora.hunter@hotmail.co.uk>
2026-03-23 09:48:56 +0000

    Release v0.2.0

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="pat2vec",
-    version="1.0.0",
-    description=(
-        "A comprehensive Python package for healthcare data engineering, designed to "
-        "extract, transform, and feature engineer patient data from CogStack-based "
-        "electronic health record (EHR) datalakes. It provides tools for cohort "
-        "building, batch data processing, clinical note analysis, and creating "
-        "machine learning-ready datasets."
-    ),
-    long_description=open("README.md", "r", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    author="Samora Hunter",
-    author_email="samorahunter@gmail.com",
-    url="https://github.com/SamoraHunter/pat2vec.git",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
